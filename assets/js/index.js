5f92c5a15a1fd3a3dea751ccf01222776338d332
20181169 <peter110998@naver.com>
2025-03-06 00:19:48 +0900

    scroll-slow5

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
 history.scrollRestoration = "manual"
 //lenis 스크롤 스무스
-const lenis = new Lenis({ wheelMultiplier: 0.85 });
+const lenis = new Lenis();
 lenis.on('scroll', ScrollTrigger.update);
 gsap.ticker.add((time) => {
-  lenis.raf(time * 800); 
+  lenis.raf(time * 900); 
 });
 gsap.ticker.lagSmoothing(0);
 
@@ -158,16 +158,16 @@ mm.add("(min-width:769px)",function(){
         x:200,
     });
     // mainprojects gsap
-    const projects = gsap.to('.projects-list', {
+    const projects = gsap.to('.projects-list',{
         scrollTrigger: {
             trigger: '.projects-sec',
             start:"0% 0%",
-            end:"100% 100%",
-            scrub: 1, // scrub 값을 1에서 2로 변경
+            end: "100% 100%",
+            scrub: 1,
             invalidateOnRefresh: true,
         },
         xPercent: -100,
-        x: function() { return (window.innerWidth - 65) * 0.3; }
+        x:function(){ return (window.innerWidth - 65) * 0.23; }
     });
     // sidepj gsap
     const sidepj = gsap.timeline({
